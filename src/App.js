094aa8ebfa5f8026b518977b5e4f908ac1a4f649
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-08-30 13:35:27 +0530

    router set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from './Components/Home'
+import AboutUs from './Components/AboutUs'
+import Services from './Components/Services'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Screen</h1>
-    </div>
+    <Router>
+
+      <div className="App">
+        <Route path='/' component={Home} exact />
+        <Route path='/aboutus' component={AboutUs} exact />
+        <Route path='/services' component={Services} exact />
+      </div>
+
+    </Router>
   );
 }
 
